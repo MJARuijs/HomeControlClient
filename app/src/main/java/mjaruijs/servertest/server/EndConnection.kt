8a8d76b1a48001106297d936d77d19cd0d917a69
Marc Ruijs <mja.ruijs@gmail.com>
2018-04-11 21:50:36 +0200

    test

--- app/src/main/java/mjaruijs/servertest/server/EndConnection.kt
+++ app/src/main/java/mjaruijs/servertest/server/EndConnection.kt
@@ -1,45 +1,28 @@
 package mjaruijs.servertest.server
 
 import android.os.AsyncTask
-import android.util.Log
+import mjaruijs.servertest.networking.networking.SecureClient
 import mjaruijs.servertest.server.authentication.ConnectionState
 import mjaruijs.servertest.server.authentication.ConnectionState.CONNECTION_CLOSED
 import mjaruijs.servertest.server.authentication.ConnectionState.NO_CONNECTION
-import java.io.DataInputStream
-import java.io.DataOutputStream
 import java.io.IOException
-import java.io.PrintWriter
-import java.net.Socket
 
 class EndConnection(private val response: ConnectionResponse) : AsyncTask<Void, Void, ConnectionState>() {
 
     override fun doInBackground(vararg params: Void?): ConnectionState {
-        val client: Socket
-        val inputStream: DataInputStream
-        val outputStream: DataOutputStream
+        val client = SecureClient("192.168.0.11", 4444)
 
-        try {
-            client = Socket("192.168.0.14", 80)
-            inputStream = DataInputStream(client.getInputStream())
-            outputStream = DataOutputStream(client.getOutputStream())
+        return try {
+            client.writeMessage("close_connection")
 
-            val printWriter = PrintWriter(outputStream)
-            printWriter.println("close_connection")
-            printWriter.flush()
+            val response = client.readMessage()
 
-            val inputLine = StringBuilder()
-            val lines = inputStream.reader().readLines()
-
-            lines.forEach { l -> inputLine.append(l) }
-
-            Log.i("EndConnection", "result: $inputLine")
-
-            inputStream.close()
-            client.close()
-            return if (inputLine.toString() == "CONNECTION_CLOSED") CONNECTION_CLOSED else NO_CONNECTION
+            if (response == "CONNECTION_CLOSED") CONNECTION_CLOSED else NO_CONNECTION
         } catch (e: IOException) {
             e.printStackTrace()
-            return NO_CONNECTION
+            NO_CONNECTION
+        } finally {
+            client.close()
         }
     }
 
